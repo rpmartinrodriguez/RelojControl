74fb8d94093038cc9e17d8cf8e240f429e1d369e
rpmartinrodriguez <rp.rodriguezmartin@gmail.com>
2026-08-31 10:14:36 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,28 @@
-self.addEventListener('fetch', function(event) {
-  // Service Worker básico para PWA
+const CACHE_NAME = 'control-horario-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Devuelve el recurso de la caché si lo encuentra, de lo contrario lo busca en red
+        return response || fetch(event.request);
+      })
+  );
 });
